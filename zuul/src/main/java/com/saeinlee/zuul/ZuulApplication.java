c66d84b26ad9ae250187e620a90ba2e9d8fb4e2c
saein-lee <saeinlee@mytalk.co.kr>
2020-07-02 18:14:29 +0900

    zuul router 설정 확인

--- zuul/src/main/java/com/saeinlee/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/saeinlee/zuul/ZuulApplication.java
@@ -6,12 +6,12 @@ import com.saeinlee.zuul.filters.PreFilter;
 import com.saeinlee.zuul.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ZuulApplication {
 
